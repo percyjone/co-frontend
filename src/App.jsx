ffc559aef2f0c75699710692cfd6839b56733732
percy jone <percyjone1234@gmail.com>
2025-01-23 21:16:51 +0530

    login component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React, { useState,useEffect } from "react";
 import "./App.css";
 import Table from "./components/QuestionMapTable.jsx";
 import MarkEntryTable from "./components/MarkEntryTable.jsx";
-import Selection from "./components/Selection.jsx";
+import Auth from "./components/auth.jsx";   
 
 const App = () => {
   return (
     <div className="App">
       <h1>Table</h1>
-      <Selection />
-      <MarkEntryTable />
+      <Auth />
+      
     </div>
   );
 };
