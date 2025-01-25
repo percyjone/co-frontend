d2e9c1fe73c04fe423cc56dd5b8a51138c14c688
percy jone <percyjone1234@gmail.com>
2025-01-25 19:13:50 +0530

    Home page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import "./App.css";
 import UploadQuestionPaper from "./pages/UploadQuestionPaper.jsx";
 import QuestionMarkEntryTable from "./components/QuestionMarkEntryTable.jsx";
 import { questions } from "./markEntryTableInput.js";
+// import Auth from "./components/Auth.jsx";
+import Home from "./pages/Home.jsx";
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Student Mark Visualization</h1>
-      <QuestionMarkEntryTable questions={questions}/>
+      {/* <h1>Student Mark Visualization</h1> */}
+      {/* <QuestionMarkEntryTable questions={questions}/>
+      <Auth/> */}
+      {/* <UploadQuestionPaper/> */}
+      <Home/>
     </div>
   );
 };
